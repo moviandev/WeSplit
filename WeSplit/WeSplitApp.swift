c3e3232d8ba06d61d43862b9be94ff927635bca9
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-08 00:23:12 -0300

    create project WeSplit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Matheus Viana on 08/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
