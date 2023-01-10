f52190b2db84ac74bd789c430355db148b63f338
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-09 23:02:44 -0300

    Add textfield to handle currency and the check amount, and a Text component to display the check amount, also we format both the text component as well the text field to be a currency and use the Locale property that's build on iOS

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name: String = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
         Form {
-            TextField("Enter your name", text: $name)
-            Text("Your name is \(name)")
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+            
+            
         }
     }
 }
